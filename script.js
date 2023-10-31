6c52939cba3ff341b27a286de315432338639c4c
Zezqi <ariski254@gmail.com>
2023-10-31 10:21:25 +0700

    modified

--- script.js
+++ script.js
@@ -3,12 +3,10 @@ const navMenu = document.getElementById("nav-menu"),
   toggleMenu = document.getElementById("nav-toggle"),
   closeMenu = document.getElementById("nav-close");
 
-// Show
 toggleMenu.addEventListener("click", () => {
   navMenu.classList.toggle("show");
 });
 
-// Hidden
 closeMenu.addEventListener("click", () => {
   navMenu.classList.remove("show");
 });
